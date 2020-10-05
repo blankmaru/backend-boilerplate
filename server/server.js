6b059cb264b103b5c906082c551ebbf4f5139f15
blankmaru <coopskywavy@gmail.com>
2020-10-05 23:26:10 +0300

    Init backend

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+app.listen(port);
